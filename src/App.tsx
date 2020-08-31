96e3abd65f59fb7538a0e7878911baea382b70ac
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-08-31 23:48:39 +0200

    add MoneySelector Component

--- src/App.tsx
+++ src/App.tsx
@@ -5,70 +5,85 @@ import Header from "./components/Header";
 
 import InputAmount from "./components/InputAmount";
 import Result from "./components/Result";
+import MoneySelector from "./components/MoneySelector";
 import "./App.scss";
 
 const AMOUNT_DELAY = 1000;
 
+const Currencies = {
+  USD: "USD - Dollar",
+  EUR: "EUR - Euro",
+  CHF: "CHF - Swiss Franc",
+};
+
 function App() {
   const [amount, setAmount] = useState<number>(0);
   const [result, setResult] = useState<number>(0);
-  const [selectedCurrency, setSelectedCurrency] = useState<any>("EURUSD");
-  const [rates, setRates] = useState<{ [key: string]: number }>({
-    EURUSD: 1.19,
-    EURCHF: 1.08,
-  });
-  const [selectedCurrencyRate, setSelectedCurrencyRate] = useState(
-    rates[selectedCurrency]
+  const [selectedCurrencyFrom, setSelectedCurrencyFrom] = useState<string>(
+    "EUR"
   );
+  const [selectedCurrencyTo, setSelectedCurrencyTo] = useState<string>("USD");
+  const [rates, setRates] = useState<{ [key: string]: number }>({});
 
   const debouncedAmount = useDebounce(amount, AMOUNT_DELAY);
 
   useEffect(() => {
-    console.log(debouncedAmount);
-    console.log(rates[selectedCurrency]);
-    setResult(debouncedAmount * rates[selectedCurrency]);
-  }, [debouncedAmount, selectedCurrency]);
+    setRates({ EURUSD: 1.19, EURCHF: 1.08 });
+  }, []);
+
+  useEffect(() => {
+    setResult(
+      rates[`${selectedCurrencyFrom}${selectedCurrencyTo}`]
+        ? rates[`${selectedCurrencyFrom}${selectedCurrencyTo}`] *
+            debouncedAmount
+        : 0
+    );
+    console.log(selectedCurrencyFrom, selectedCurrencyTo);
+  }, [debouncedAmount, selectedCurrencyFrom, selectedCurrencyTo]);
 
   return (
     <div>
       <Header />
       <Container className="container">
         <Grid container spacing={3}>
-          <Grid item xs={2}>
-            <small>Base Currency</small>
-            <strong>EUR</strong>
+          <Grid item xs={12}>
+            <h2>Currency Converter</h2>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={5}>
+            <MoneySelector
+              label="From"
+              values={Currencies}
+              selectedCurrency={selectedCurrencyFrom}
+              onSelectChange={(
+                event: React.ChangeEvent<{ value: unknown }>
+              ) => {
+                setSelectedCurrencyFrom(event.target.value as string);
+              }}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <button>change</button>
+          </Grid>
+          <Grid item xs={5}>
+            <MoneySelector
+              label="To"
+              values={Currencies}
+              selectedCurrency={selectedCurrencyTo}
+              onSelectChange={(
+                event: React.ChangeEvent<{ value: unknown }>
+              ) => {
+                setSelectedCurrencyTo(event.target.value as string);
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
             <InputAmount
               onAmountChange={(e: any) => {
                 setAmount(e.target.value);
               }}
             />
           </Grid>
-          <Grid item xs={2}>
-            <Paper
-              onClick={() => {
-                setSelectedCurrency("EURUSD");
-              }}
-              elevation={3}
-              className="currency-selector"
-            >
-              <span>$</span>
-              <span>USD</span>
-            </Paper>
-          </Grid>
-          <Grid item xs={2}>
-            <Paper
-              onClick={() => {
-                setSelectedCurrency("EURCHF");
-              }}
-              elevation={3}
-              className="currency-selector"
-            >
-              <span>ƒ</span>
-              <span>CHF</span>
-            </Paper>
-          </Grid>
+
           <Grid item xs={12}>
             <Result data={result} />
           </Grid>
---
+++ src/components/MoneySelector/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
+} from "@material-ui/core";
+
+interface Props {
+  label: string;
+  onSelectChange: any;
+  values: {
+    [key: string]: string;
+  };
+  selectedCurrency: string;
+}
+
+const MoneySelector: React.FC<Props> = ({
+  label,
+  values,
+  selectedCurrency,
+  onSelectChange,
+}) => {
+  return (
+    <FormControl fullWidth>
+      <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+        {label}
+      </InputLabel>
+      <Select value={selectedCurrency} onChange={onSelectChange}>
+        {Object.keys(values).map((item) => (
+          <MenuItem key={item} value={item}>
+            {values[item]}
+          </MenuItem>
+        ))}
+      </Select>
+      <FormHelperText>Label + placeholder</FormHelperText>
+    </FormControl>
+  );
+};
+
+export default MoneySelector;
