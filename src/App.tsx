8dc8322cc51156cb94ba7f728a61ca8c5b2779df
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-08-31 18:21:38 +0200

    base App functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Props } from "react";
+import Header from "./components/Header";
+import { Container, Grid, Paper } from "@material-ui/core";
+import InputAmount from "./components/InputAmount";
+import Result from "./components/Result";
+import "./App.scss";
 
 function App() {
+  const [amount, setAmount] = useState<number>(0);
+  const [result, setResult] = useState<number>(0);
+  const [selectedCurrency, setSelectedCurrency] = useState<any>("EURUSD");
+  const [rates, setRates] = useState<{ [key: string]: number }>({
+    EURUSD: 1.19,
+    EURCHF: 1.08,
+  });
+  const [selectedCurrencyRate, setSelectedCurrencyRate] = useState(
+    rates[selectedCurrency]
+  );
+
+  useEffect(() => {
+    console.log(amount);
+    console.log(rates[selectedCurrency]);
+    setResult(amount * rates[selectedCurrency]);
+  }, [amount, selectedCurrency]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Container className="container">
+        <Grid container spacing={3}>
+          <Grid item xs={2}>
+            <small>Base Currency</small>
+            <strong>EUR</strong>
+          </Grid>
+          <Grid item xs={6}>
+            <InputAmount
+              onAmountChange={(e: any) => {
+                setAmount(e.target.value);
+              }}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <Paper
+              onClick={() => {
+                setSelectedCurrency("EURUSD");
+              }}
+              elevation={3}
+              className="currency-selector"
+            >
+              <span>$</span>
+              <span>USD</span>
+            </Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper
+              onClick={() => {
+                setSelectedCurrency("EURCHF");
+              }}
+              elevation={3}
+              className="currency-selector"
+            >
+              <span>ƒ</span>
+              <span>CHF</span>
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Result data={result} />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
