d7bf402abb8eea3d170bbfec11d08c39ec56902f
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-09-01 12:02:49 +0200

    add sass to Result Component

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "./Result.scss";
 interface Props {
   data: number;
 }
 
 const Result: React.FC<Props> = ({ data }) => {
   return (
-    <div>
+    <div className="result">
       <span>$</span>
       <p> {data} </p>
     </div>
