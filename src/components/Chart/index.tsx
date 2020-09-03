5b69ddd9494bc20a9a7c4e5faf81ad4b3497b0bc
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-09-04 00:23:16 +0200

    add currency data to the chart

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,186 +1,193 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ResponsiveLine } from "@nivo/line";
+import moment from "moment";
+const CurrencyLayerClient = require("currencylayer-client");
 
-const Chart = () => {
-  const data = [
-    {
-      id: "Euro to USD",
-      color: "hsl(11, 70%, 50%)",
-      data: [
-        {
-          x: "2020-09-01",
-          y: 285,
-        },
-        {
-          x: "2020-08-31",
-          y: 114,
-        },
-        {
-          x: "2020-08-30",
-          y: 229,
-        },
-        {
-          x: "2020-08-29",
-          y: 267,
-        },
-        {
-          x: "2020-08-28",
-          y: 236,
-        },
-        {
-          x: "2020-08-27",
-          y: 199,
-        },
-        {
-          x: "2020-08-26",
-          y: 281,
-        },
-        {
-          x: "2020-08-25",
-          y: 133,
-        },
-        {
-          x: "2020-08-24",
-          y: 140,
-        },
-        {
-          x: "2020-08-23",
-          y: 96,
-        },
-        {
-          x: "2020-08-22",
-          y: 270,
-        },
-        {
-          x: "2020-08-21",
-          y: 244,
-        },
-      ],
-    },
-    {
-      id: "Euro to CHF",
-      color: "hsl(270, 70%, 50%)",
-      data: [
-        {
-          x: "2020-09-01",
-          y: 291,
-        },
-        {
-          x: "2020-08-31",
-          y: 287,
-        },
-        {
-          x: "2020-08-30",
-          y: 217,
-        },
-        {
-          x: "2020-08-29",
-          y: 80,
-        },
-        {
-          x: "2020-08-28",
-          y: 0,
-        },
-        {
-          x: "2020-08-27",
-          y: 185,
-        },
-        {
-          x: "2020-08-26",
-          y: 219,
-        },
-        {
-          x: "2020-08-25",
-          y: 59,
-        },
-        {
-          x: "2020-08-24",
-          y: 25,
-        },
-        {
-          x: "2020-08-23",
-          y: 3,
-        },
-        {
-          x: "2020-08-22",
-          y: 260,
-        },
-        {
-          x: "2020-08-21",
-          y: 83,
-        },
-      ],
-    },
-  ];
+const client = new CurrencyLayerClient({
+  apiKey: "7cb2fa306d15983c0edcec597fa8234dd",
+});
+
+type Props = {
+  selectedCurrencyFrom: string;
+  selectedCurrencyTo: string;
+};
+
+const Chart: React.FC<Props> = ({
+  selectedCurrencyFrom,
+  selectedCurrencyTo,
+}) => {
+  const [twoWeeksArray, setTwoWeeksArray] = useState<string[]>(
+    Array(2)
+      .fill("")
+      .map((_, i) => moment().subtract(i, "d").format("YYYY-MM-DD"))
+  );
+
+  const [
+    historicalConvertionRateArray,
+    setHistoricalConvertionRateArray,
+  ] = useState([]);
+  const [chartData, setChartData] = useState<
+    Array<{
+      id: string;
+      color: string;
+      data: any[];
+    }>
+  >();
+
+  useEffect(() => {
+    generateDailyConvertionRate(twoWeeksArray);
+  }, []);
+
+  useEffect(() => {
+    if (historicalConvertionRateArray.length)
+      generateChartDate(
+        selectedCurrencyFrom,
+        selectedCurrencyTo,
+        historicalConvertionRateArray
+      );
+  }, [
+    historicalConvertionRateArray.length,
+    selectedCurrencyFrom,
+    selectedCurrencyTo,
+  ]);
+
+  const getHistoryRate = (date: string) => {
+    let rates: any = {};
+    let allRates: any = {};
+
+    return client
+      .historical({ date, currencies: "CHF,EUR", source: "USD" })
+      .then((data: any) => {
+        console.log("fetched new data ", data);
+        rates = data.quotes;
+
+        allRates = {
+          ...rates,
+          EURUSD: Number((1 / rates.USDEUR).toFixed(3)),
+          CHFUSD: Number((1 / rates.USDCHF).toFixed(3)),
+          EURCHF: Number(((1 / rates.USDEUR) * rates.USDCHF).toFixed(3)),
+          CHFEUR: Number(((1 / rates.USDCHF) * rates.USDEUR).toFixed(3)),
+        };
+
+        return allRates;
+      })
+      .catch((err: any) => {
+        console.log(err.code); // 104
+        console.log(err.message); // Your monthly usage limit has been reached...
+      });
+  };
+
+  const generateDailyConvertionRate = async (array: string[]) => {
+    const historicalConvertionRateDate: any = [];
+
+    await array.map(async (item: any) => {
+      let rates: any;
+      await getHistoryRate(item).then((data: any) => {
+        rates = data;
+
+        historicalConvertionRateDate.push({
+          date: item,
+          rates: { ...rates },
+        });
+      });
+    });
+    setHistoricalConvertionRateArray(historicalConvertionRateDate);
+  };
+
+  const generateChartDate = (
+    selectedCurrencyFrom: string,
+    selectedCurrencyTo: string,
+    historicalRateArray: object[]
+  ) => {
+    let data: Array<{ x: string; y: number }> = [];
+    console.log(data);
+    historicalRateArray.map((item: any) => {
+      data.push({
+        x: item.date,
+        y: item.rates[selectedCurrencyFrom + selectedCurrencyTo],
+      });
+    });
+
+    setChartData([
+      {
+        id: `${selectedCurrencyFrom} to ${selectedCurrencyTo}`,
+        color: "hsl(11, 70%, 50%)",
+        data: [...data],
+      },
+    ]);
+  };
 
   return (
     <div style={{ height: "300px" }}>
-      <ResponsiveLine
-        data={data}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: "point" }}
-        yScale={{
-          type: "linear",
-          min: "auto",
-          max: "auto",
-          stacked: true,
-          reverse: false,
-        }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          orient: "bottom",
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "date",
-          legendOffset: 36,
-          legendPosition: "middle",
-        }}
-        axisLeft={{
-          orient: "left",
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "Price",
-          legendOffset: -40,
-          legendPosition: "middle",
-        }}
-        colors={{ scheme: "nivo" }}
-        pointSize={10}
-        pointColor={{ theme: "background" }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: "serieColor" }}
-        pointLabel="y"
-        pointLabelYOffset={-12}
-        useMesh={true}
-        legends={[
-          {
-            anchor: "bottom-right",
-            direction: "column",
-            justify: false,
-            translateX: 100,
-            translateY: 0,
-            itemsSpacing: 0,
-            itemDirection: "left-to-right",
-            itemWidth: 80,
-            itemHeight: 20,
-            itemOpacity: 0.75,
-            symbolSize: 12,
-            symbolShape: "circle",
-            symbolBorderColor: "rgba(0, 0, 0, .5)",
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemBackground: "rgba(0, 0, 0, .03)",
-                  itemOpacity: 1,
+      {chartData && (
+        <ResponsiveLine
+          data={chartData}
+          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+          xScale={{ type: "point" }}
+          yScale={{
+            type: "linear",
+            min: "auto",
+            max: "auto",
+            stacked: false,
+            reverse: false,
+          }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            orient: "bottom",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "date",
+            legendOffset: 36,
+            legendPosition: "middle",
+          }}
+          axisLeft={{
+            orient: "left",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Price",
+            legendOffset: -40,
+            legendPosition: "middle",
+          }}
+          colors={{ scheme: "nivo" }}
+          pointSize={10}
+          pointColor={{ theme: "background" }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: "serieColor" }}
+          pointLabel="y"
+          pointLabelYOffset={-12}
+          enableSlices="x"
+          useMesh={true}
+          legends={[
+            {
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 100,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemDirection: "left-to-right",
+              itemWidth: 80,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: "circle",
+              symbolBorderColor: "rgba(0, 0, 0, .5)",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemBackground: "rgba(0, 0, 0, .03)",
+                    itemOpacity: 1,
+                  },
                 },
-              },
-            ],
-          },
-        ]}
-      />
+              ],
+            },
+          ]}
+        />
+      )}
     </div>
   );
 };
