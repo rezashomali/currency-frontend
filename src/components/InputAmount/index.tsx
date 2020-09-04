af908e00537399c4a0f140ad5dfc31c49d96c81c
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-09-04 15:02:06 +0200

    add validations for inputAmount component

--- src/components/InputAmount/index.tsx
+++ src/components/InputAmount/index.tsx
@@ -10,16 +10,20 @@ import "./InputAmount.scss";
 
 type Props = InputProps & {
   symbol: string;
+  amount: number;
+  helperText?: string;
 };
 
-const InputAmount: React.FC<Props> = ({ onChange, symbol }) => {
+const InputAmount: React.FC<Props> = ({ onChange, symbol, amount }) => {
   return (
     <FormControl fullWidth className="inputAmount">
       <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
       <Input
+        type="number"
+        error={amount < 0 ? true : false}
         className="inputAmount__input"
         // id="standard-adornment-amount"
-        // value={values.amount}
+        value={amount}
         onChange={onChange}
         startAdornment={
           <InputAdornment className="inputAmount__symbol" position="start">
@@ -27,6 +31,11 @@ const InputAmount: React.FC<Props> = ({ onChange, symbol }) => {
           </InputAdornment>
         }
       />
+      {amount < 0 ? (
+        <span className="inputAmount__error">incorect value</span>
+      ) : (
+        ""
+      )}
     </FormControl>
   );
 };
